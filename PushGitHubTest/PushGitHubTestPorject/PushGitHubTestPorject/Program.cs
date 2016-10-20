4cc30873e33f71a0d015275a2c347473e10c2df9
ljxin <ljxin@Ctrip.com>
2016-10-20 09:58:43 +0800

    测试项目提交

---
+++ PushGitHubTest/PushGitHubTestPorject/PushGitHubTestPorject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushGitHubTestPorject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("推送本地代码到GitHub测试!");
+            Console.ReadKey();
+        }
+    }
+}
